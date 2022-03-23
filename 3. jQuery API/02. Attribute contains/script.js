0e1df50ea60f830c0d18537d6d605dee695c0413
Bùi Hữu Lộc <feature451@gmail.com>
2022-03-23 21:27:24 +0700

    new lesson: attribute contains selector

---
+++ 3. jQuery API/02. Attribute contains/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Tìm tất cả thẻ a có giá trị của thuộc tính name có chứa chữ 'name'
+    // $("a[name*='link']").css('color', 'red')
+    
+    // Tìm tất cả thẻ a có giá trị của thuộc tính class bắt đầu bằng chữ 'link'
+    // $("a[class^='link']").css('color', 'yellow')
+  
+    // Tìm tất cả thẻ a có giá trị của thuộc tính id kết thúc bằng chữ 'link'
+    // $("a[id$='link']").css('color', 'violet')
+
+    // Tìm tất cả thẻ a có giá trị của thuộc tính value là 'link', phân biệt hoa thường
+    // $("a[value='link']").css('color', 'yellow')
+
+    // Tìm tất cả thẻ a có giá trị của thuộc tính value khác với chữ 'link'
+    // $("a[value!='link']").css('color', 'grey')
+
+})
